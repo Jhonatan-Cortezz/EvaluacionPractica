4a743a1097f29329ad564b18b6112dc82717c1f3
CrisMrtz01 <kooper8273@gmail.com>
2020-10-03 20:04:28 -0600

    Clase Usuario

---
+++ app/src/main/java/com/jac/evaluacionpractica/Usuario.java
@@ -0,0 +1,125 @@
+package com.jac.evaluacionpractica;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Usuario implements Serializable{
+    int id;
+    String nombre;
+    String apellido;
+    String correo;
+    String usuario;
+    String clave;
+    int tipo;
+    int estado;
+    String pregunta;
+    String respuesta;
+    Timestamp fecha_registro;
+
+    public Usuario(){
+
+    }
+
+
+    public Usuario(int id, String nombre, String apellido, String correo, String usuario, String clave, int tipo, int estado, String pregunta, String respuesta, Timestamp fecha_registro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+        this.fecha_registro = fecha_registro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public Timestamp getFecha_registro() {
+        return fecha_registro;
+    }
+
+    public void setFecha_registro(Timestamp fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
+}
